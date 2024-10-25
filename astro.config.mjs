b13b9842c7be98c30cc0f46ac9307fd4e2bd3b8a
jeremiaranggasetyawan <jeremiaranggas@gmail.com>
2024-10-25 11:51:43 +0700

    randomize card

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,6 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://bangranggas.github.io',
   base: 'kolpri',
+  outDir: 'public',
+  publicDir: 'static',
 });
