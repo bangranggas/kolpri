77e9e06349188a30701960ef32b771fddd1373b7
jeremiaranggasetyawan <jeremiaranggas@gmail.com>
2024-10-25 11:53:10 +0700

    fixing config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,4 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://bangranggas.github.io',
   base: 'kolpri',
-  outDir: 'public',
-  publicDir: 'static',
 });
